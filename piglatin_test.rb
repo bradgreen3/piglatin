47346cca4289a8d87b59fa5bc43804332d17b6f1
Brad <bgreen3@uoregon.edu>
2017-02-23 23:03:21 -0700

    Add initialization with english in order to remove self methods

--- piglatin_test.rb
+++ piglatin_test.rb
@@ -5,34 +5,34 @@ require 'minitest/pride'
 
 class PigLatinTest < Minitest::Test
   def test_1
-    assert_equal 'arrowway', PigLatin.translate('arrow')
+    assert_equal 'arrowway', PigLatin.new('arrow').translate
   end
 
   def test_2
-    assert_equal 'igpay', PigLatin.translate('pig')
+    assert_equal 'igpay', PigLatin.new('pig').translate
   end
 
   def test_3
-    assert_equal 'igpay arrowway', PigLatin.translate('pig arrow')
+    assert_equal 'igpay arrowway', PigLatin.new('pig arrow').translate
   end
 
   def test_4
-    assert_equal 'appleway ananabay orangeway', PigLatin.translate('apple banana orange')
+    assert_equal 'appleway ananabay orangeway', PigLatin.new('apple banana orange').translate
   end
 
   def test_5
-    assert_equal 'oughthray', PigLatin.translate('through')
+    assert_equal 'oughthray', PigLatin.new('through').translate
   end
 
   def test_6
-    assert_equal 'oughthray', PigLatin.translate('ThRoUgH')
+    assert_equal 'oughthray', PigLatin.new('ThRoUgH').translate
   end
 
   def test_7
-    assert_equal 'oughthray appleway igpay', PigLatin.translate('THROUGH apple PIG')
+    assert_equal 'oughthray appleway igpay', PigLatin.new('THROUGH apple PIG').translate
   end
 
   def test_8
-    assert_equal 'appleway ananabay oughthray', PigLatin.translate('apple, banana, through')
+    assert_equal 'appleway ananabay oughthray', PigLatin.new('apple, banana, through').translate
   end
 end
